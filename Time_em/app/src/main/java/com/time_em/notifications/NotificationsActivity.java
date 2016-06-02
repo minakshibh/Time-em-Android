86dc39d0af9028e1cc9a85202bd3ced1ffe0f97c
BalwinderKis <balwindersingh@krishnais.com>
2016-06-02 14:20:05 +0530

    Work

---
+++ Time_em/app/src/main/java/com/time_em/notifications/NotificationsActivity.java
@@ -0,0 +1,17 @@
+package com.time_em.notifications;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.time_em.android.R;
+
+public class NotificationsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_send_notification);
+
+    }
+}
