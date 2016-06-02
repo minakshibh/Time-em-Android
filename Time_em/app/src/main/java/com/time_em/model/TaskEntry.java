a1062bf0d238e8dae17b951c6c58748e8457c5b0
BalwinderKis <balwindersingh@krishnais.com>
2016-06-02 10:45:10 +0530

    Worked on task Details

--- Time_em/app/src/main/java/com/time_em/model/TaskEntry.java
+++ Time_em/app/src/main/java/com/time_em/model/TaskEntry.java
@@ -3,153 +3,185 @@ package com.time_em.model;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-public class TaskEntry implements Parcelable{
-	
-	private int id, activityId, taskId, userId;
-	private String taskName, comments, startTime, createdDate, endTime, selectedDate, token;
-	private Double timeSpent, signedInHours;
-	private Boolean isActive = true;
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public int getActivityId() {
-		return activityId;
-	}
-	public void setActivityId(int activityId) {
-		this.activityId = activityId;
-	}
-	public int getTaskId() {
-		return taskId;
-	}
-	public void setTaskId(int taskId) {
-		this.taskId = taskId;
-	}
-	public int getUserId() {
-		return userId;
-	}
-	public void setUserId(int userId) {
-		this.userId = userId;
-	}
-	public String getTaskName() {
-		return taskName;
-	}
-	public void setTaskName(String taskName) {
-		this.taskName = taskName;
-	}
-	public String getComments() {
-		return comments;
-	}
-	public void setComments(String comments) {
-		this.comments = comments;
-	}
-	public String getStartTime() {
-		return startTime;
-	}
-	public void setStartTime(String startTime) {
-		this.startTime = startTime;
-	}
-	public String getCreatedDate() {
-		return createdDate;
-	}
-	public void setCreatedDate(String createdDate) {
-		this.createdDate = createdDate;
-	}
-	public String getEndTime() {
-		return endTime;
-	}
-	public void setEndTime(String endTime) {
-		this.endTime = endTime;
-	}
-	public String getSelectedDate() {
-		return selectedDate;
-	}
-	public void setSelectedDate(String selectedDate) {
-		this.selectedDate = selectedDate;
-	}
-	public String getToken() {
-		return token;
-	}
-	public void setToken(String token) {
-		this.token = token;
-	}
-	public Double getTimeSpent() {
-		return timeSpent;
-	}
-	public void setTimeSpent(Double timeSpent) {
-		this.timeSpent = timeSpent;
-	}
-	public Double getSignedInHours() {
-		return signedInHours;
-	}
-	public void setSignedInHours(Double signedInHours) {
-		this.signedInHours = signedInHours;
-	}
-	public Boolean getIsActive() {
-		return isActive;
-	}
-	public void setIsActive(Boolean isActive) {
-		this.isActive = isActive;
-	}
+public class TaskEntry implements Parcelable {
 
-	public TaskEntry(){
+    private int id, activityId, taskId, userId;
+    private String taskName, comments, startTime, createdDate, endTime, selectedDate, token;
+    private Double timeSpent, signedInHours;
+    private Boolean isActive = true;
+    private String AttachmentImageFile;
 
-	}
-	public TaskEntry(Parcel source) {
-		id = source.readInt();
-		activityId = source.readInt();
-		taskId = source.readInt();
-		userId = source.readInt();
-		taskName = source.readString();
-		comments = source.readString();
-		startTime = source.readString();
-		createdDate = source.readString();
-		endTime = source.readString();
-		selectedDate = source.readString();
-		token = source.readString();
-		timeSpent = source.readDouble();
-		signedInHours = source.readDouble();
-		isActive = source.readByte() != 0;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public static final Parcelable.Creator<TaskEntry> CREATOR
-			= new Parcelable.Creator<TaskEntry>()
-	{
-		public TaskEntry createFromParcel(Parcel in)
-		{
-			return new TaskEntry(in);
-		}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-		public TaskEntry[] newArray (int size)
-		{
-			return new TaskEntry[size];
-		}
-	};
+    public int getActivityId() {
+        return activityId;
+    }
 
-	@Override
-	public int describeContents() {
-		// TODO Auto-generated method stub
-		return this.hashCode();
-	}
+    public void setActivityId(int activityId) {
+        this.activityId = activityId;
+    }
 
-	@Override
-	public void writeToParcel(Parcel dest, int flags) {
-		dest.writeInt(id);
-		dest.writeInt(activityId);
-		dest.writeInt(taskId);
-		dest.writeInt(userId);
-		dest.writeString(taskName);
-		dest.writeString(comments);
-		dest.writeString(startTime);
-		dest.writeString(createdDate);
-		dest.writeString(endTime);
-		dest.writeString(selectedDate);
-		dest.writeString(token);
-		dest.writeDouble(timeSpent);
-		dest.writeDouble(signedInHours);
-		dest.writeByte((byte) (isActive ? 1 : 0));
-	}
+    public int getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(int taskId) {
+        this.taskId = taskId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getSelectedDate() {
+        return selectedDate;
+    }
+
+    public void setSelectedDate(String selectedDate) {
+        this.selectedDate = selectedDate;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Double getTimeSpent() {
+        return timeSpent;
+    }
+
+    public void setTimeSpent(Double timeSpent) {
+        this.timeSpent = timeSpent;
+    }
+
+    public Double getSignedInHours() {
+        return signedInHours;
+    }
+
+    public void setSignedInHours(Double signedInHours) {
+        this.signedInHours = signedInHours;
+    }
+
+    public Boolean getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(Boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    public void SetAttachementImageFile(String AttachmentImageFile) { this.AttachmentImageFile = AttachmentImageFile;  }
+
+    public String GetAttachementImageFile() {  return AttachmentImageFile; }
+
+    public TaskEntry() {
+
+    }
+
+    public TaskEntry(Parcel source) {
+        id = source.readInt();
+        activityId = source.readInt();
+        taskId = source.readInt();
+        userId = source.readInt();
+        taskName = source.readString();
+        comments = source.readString();
+        startTime = source.readString();
+        createdDate = source.readString();
+        endTime = source.readString();
+        selectedDate = source.readString();
+        AttachmentImageFile = source.readString();
+        token = source.readString();
+        timeSpent = source.readDouble();
+        signedInHours = source.readDouble();
+        isActive = source.readByte() != 0;
+    }
+
+    public static final Parcelable.Creator<TaskEntry> CREATOR
+            = new Parcelable.Creator<TaskEntry>() {
+        public TaskEntry createFromParcel(Parcel in) {
+            return new TaskEntry(in);
+        }
+
+        public TaskEntry[] newArray(int size) {
+            return new TaskEntry[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        // TODO Auto-generated method stub
+        return this.hashCode();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeInt(activityId);
+        dest.writeInt(taskId);
+        dest.writeInt(userId);
+        dest.writeString(taskName);
+        dest.writeString(comments);
+        dest.writeString(startTime);
+        dest.writeString(createdDate);
+        dest.writeString(endTime);
+        dest.writeString(selectedDate);
+        dest.writeString(token);
+        dest.writeString(AttachmentImageFile);
+        dest.writeDouble(timeSpent);
+        dest.writeDouble(signedInHours);
+        dest.writeByte((byte) (isActive ? 1 : 0));
+    }
 }
